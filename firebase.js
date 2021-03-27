30275f0418c209fd9b16dc30392ee550db18f14d
geof <>
2021-03-27 11:21:39 +0300

    added firebase, material ui & icons, started on sidebar component

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6jD02Ct7NGfs3k84Q4SDnoRrO5dXfFE8",
+    authDomain: "whatsapp-nextjs.firebaseapp.com",
+    projectId: "whatsapp-nextjs",
+    storageBucket: "whatsapp-nextjs.appspot.com",
+    messagingSenderId: "527317073667",
+    appId: "1:527317073667:web:25655ac26bd2f28e563336"
+  };
+
+  const app
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Whatsapp-Nextjs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>Let's get started</h1>
+     <Sidebar/>
     </div>
   )
 }
